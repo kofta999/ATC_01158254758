c9bcbe66539b833009b7122272d0ff121b31f195
kofta999 <mostafaxxx555@gmail.com>
2025-05-10 20:19:07 +0300

    chore(backend): add events seed

--- apps/backend/database/schema/seed.sql
+++ apps/backend/database/schema/seed.sql
@@ -8,6 +8,46 @@ VALUES
         'USER'
     );
 
+-- Insert events
+INSERT INTO
+    "event" (
+        event_name,
+        description,
+        category,
+        date,
+        venue,
+        price,
+        image
+    )
+VALUES
+    (
+        'Summer Music Festival',
+        'A vibrant music festival featuring top artists.',
+        'Music',
+        '2024-07-20',
+        'Central Park, New York',
+        50,
+        'url-to-image1'
+    ),
+    (
+        'Tech Conference 2024',
+        'The leading tech conference of the year.',
+        'Technology',
+        '2024-09-15',
+        'San Francisco, CA',
+        200,
+        'url-to-image2'
+    ),
+    (
+        'Foodies Festival',
+        'A culinary experience for food lovers.',
+        'Food',
+        '2024-11-10',
+        'Miami, FL',
+        30,
+        'url-to-image3'
+    );
+
 -- Insert admins (password is 12345678)
 INSERT INTO
     "user" (email, password, role)
