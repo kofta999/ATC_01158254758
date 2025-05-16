6ffa661f8194906d15a6199401a1da201c49b43e
kofta999 <mostafaxxx555@gmail.com>
2025-05-16 12:41:48 +0300

    test(backend): improve event service tests

--- apps/backend/test/unit/core/application/services/event.service.test.ts
+++ apps/backend/test/unit/core/application/services/event.service.test.ts
@@ -53,6 +53,7 @@ describe("Event service", () => {
 			];
 
 			mockEventRepo.getAll.mockResolvedValueOnce(mockEvents);
+			mockEventRepo.count.mockResolvedValueOnce(mockEvents.length);
 
 			const pagination = { limit: mockEvents.length, page: 1 };
 			const events = await service.getEventList({
@@ -69,6 +70,307 @@ describe("Event service", () => {
 					hasPreviousPage: false,
 				},
 			});
+			expect(mockEventRepo.getAll).toHaveBeenCalledTimes(1);
+		});
+
+		it("Should return a list of events with pagination", async () => {
+			const mockEvents = [
+				new Event({
+					eventId: 1,
+					eventName: "Event 1",
+					description: "Desc 1",
+					category: "Business",
+					date: "2024-01-01",
+					venue: "Venue 1",
+					price: 10,
+					image: "img1.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 2,
+					eventName: "Event 2",
+					description: "Desc 2",
+					category: "Music",
+					date: "2024-01-02",
+					venue: "Venue 2",
+					price: 20,
+					image: "img2.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 3,
+					eventName: "Event 3",
+					description: "Desc 3",
+					category: "Sports",
+					date: "2024-01-03",
+					venue: "Venue 3",
+					price: 30,
+					image: "img3.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 4,
+					eventName: "Event 4",
+					description: "Desc 4",
+					category: "Technology",
+					date: "2024-01-04",
+					venue: "Venue 4",
+					price: 40,
+					image: "img4.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 5,
+					eventName: "Event 5",
+					description: "Desc 5",
+					category: "Business",
+					date: "2024-01-05",
+					venue: "Venue 5",
+					price: 50,
+					image: "img5.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 6,
+					eventName: "Event 6",
+					description: "Desc 6",
+					category: "Music",
+					date: "2024-01-06",
+					venue: "Venue 6",
+					price: 60,
+					image: "img6.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 7,
+					eventName: "Event 7",
+					description: "Desc 7",
+					category: "Sports",
+					date: "2024-01-07",
+					venue: "Venue 7",
+					price: 70,
+					image: "img7.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 8,
+					eventName: "Event 8",
+					description: "Desc 8",
+					category: "Technology",
+					date: "2024-01-08",
+					venue: "Venue 8",
+					price: 80,
+					image: "img8.jpg",
+					availableTickets: 100,
+				}),
+			];
+			const paginatedEvents = [
+				new Event({
+					eventId: 6,
+					eventName: "Event 6",
+					description: "Desc 6",
+					category: "Music",
+					date: "2024-01-06",
+					venue: "Venue 6",
+					price: 60,
+					image: "img6.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 7,
+					eventName: "Event 7",
+					description: "Desc 7",
+					category: "Sports",
+					date: "2024-01-07",
+					venue: "Venue 7",
+					price: 70,
+					image: "img7.jpg",
+					availableTickets: 100,
+				}),
+				new Event({
+					eventId: 8,
+					eventName: "Event 8",
+					description: "Desc 8",
+					category: "Technology",
+					date: "2024-01-08",
+					venue: "Venue 8",
+					price: 80,
+					image: "img8.jpg",
+					availableTickets: 100,
+				}),
+			];
+
+			mockEventRepo.count.mockResolvedValueOnce(mockEvents.length);
+			mockEventRepo.getAll.mockResolvedValueOnce(paginatedEvents);
+
+			const pagination = { limit: 5, page: 2 };
+			const events = await service.getEventList({
+				pagination,
+			});
+
+			expect(events).toEqual({
+				data: paginatedEvents,
+				meta: {
+					totalItems: mockEvents.length,
+					totalPages: Math.ceil(mockEvents.length / pagination.limit),
+					currentPage: pagination.page,
+					hasNextPage: false,
+					hasPreviousPage: true,
+				},
+			});
+			expect(mockEventRepo.count).toHaveBeenCalledWith({
+				limit: 5,
+				offset: 5,
+				category: undefined,
+			});
+			expect(mockEventRepo.getAll).toHaveBeenCalledWith({
+				limit: 5,
+				offset: 5,
+				category: undefined,
+			});
+			expect(mockEventRepo.count).toHaveBeenCalledTimes(1);
+			expect(mockEventRepo.getAll).toHaveBeenCalledTimes(1);
+		});
+
+		it("Should return a list of events filtered by category", async () => {
+			const allEvents = [
+				new Event({
+					eventId: 1,
+					eventName: "Business Event 1",
+					description: "Business Desc 1",
+					category: "Business",
+					date: "2024-02-01",
+					venue: "Business Venue 1",
+					price: 100,
+					image: "business1.jpg",
+					availableTickets: 50,
+				}),
+				new Event({
+					eventId: 2,
+					eventName: "Music Event 1",
+					description: "Music Desc 1",
+					category: "Music",
+					date: "2024-02-02",
+					venue: "Music Venue 1",
+					price: 200,
+					image: "music1.jpg",
+					availableTickets: 75,
+				}),
+				new Event({
+					eventId: 3,
+					eventName: "Business Event 2",
+					description: "Business Desc 2",
+					category: "Business",
+					date: "2024-02-03",
+					venue: "Business Venue 2",
+					price: 150,
+					image: "business2.jpg",
+					availableTickets: 60,
+				}),
+				new Event({
+					eventId: 4,
+					eventName: "Music Event 2",
+					description: "Music Desc 2",
+					category: "Music",
+					date: "2024-02-04",
+					venue: "Music Venue 2",
+					price: 250,
+					image: "music2.jpg",
+					availableTickets: 80,
+				}),
+			];
+			const businessEvents = [
+				new Event({
+					eventId: 1,
+					eventName: "Business Event 1",
+					description: "Business Desc 1",
+					category: "Business",
+					date: "2024-02-01",
+					venue: "Business Venue 1",
+					price: 100,
+					image: "business1.jpg",
+					availableTickets: 50,
+				}),
+				new Event({
+					eventId: 3,
+					eventName: "Business Event 2",
+					description: "Business Desc 2",
+					category: "Business",
+					date: "2024-02-03",
+					venue: "Business Venue 2",
+					price: 150,
+					image: "business2.jpg",
+					availableTickets: 60,
+				}),
+			];
+
+			mockEventRepo.count.mockResolvedValueOnce(businessEvents.length);
+			mockEventRepo.getAll.mockResolvedValueOnce(businessEvents);
+
+			const pagination = { limit: 10, page: 1 };
+			const filter = { category: "Business" as const };
+			const events = await service.getEventList({
+				pagination,
+				filter,
+			});
+
+			expect(events).toEqual({
+				data: businessEvents,
+				meta: {
+					totalItems: businessEvents.length,
+					totalPages: Math.ceil(businessEvents.length / pagination.limit),
+					currentPage: pagination.page,
+					hasNextPage: false,
+					hasPreviousPage: false,
+				},
+			});
+			expect(mockEventRepo.count).toHaveBeenCalledWith({
+				limit: 10,
+				offset: 0,
+				category: "Business",
+			});
+			expect(mockEventRepo.getAll).toHaveBeenCalledWith({
+				limit: 10,
+				offset: 0,
+				category: "Business",
+			});
+			expect(mockEventRepo.count).toHaveBeenCalledTimes(1);
+			expect(mockEventRepo.getAll).toHaveBeenCalledTimes(1);
+		});
+
+		it("Should return an empty list and correct meta if no events match the criteria", async () => {
+			mockEventRepo.count.mockResolvedValueOnce(0);
+			mockEventRepo.getAll.mockResolvedValueOnce([]);
+
+			const pagination = { limit: 10, page: 1 };
+			const filter = { category: "Nonexistent" as const };
+			const events = await service.getEventList({
+				pagination,
+				// @ts-expect-error Custom category for tests
+				filter,
+			});
+
+			expect(events).toEqual({
+				data: [],
+				meta: {
+					totalItems: 0,
+					totalPages: 0,
+					currentPage: pagination.page,
+					hasNextPage: false,
+					hasPreviousPage: false,
+				},
+			});
+			expect(mockEventRepo.count).toHaveBeenCalledWith({
+				limit: 10,
+				offset: 0,
+				category: "Nonexistent",
+			});
+			expect(mockEventRepo.getAll).toHaveBeenCalledWith({
+				limit: 10,
+				offset: 0,
+				category: "Nonexistent",
+			});
+			expect(mockEventRepo.count).toHaveBeenCalledTimes(1);
 			expect(mockEventRepo.getAll).toHaveBeenCalledTimes(1);
 		});
 	});
